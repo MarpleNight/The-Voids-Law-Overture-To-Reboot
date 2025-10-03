4c3f6882d32a299696d0c67b433320c0000b868c
Morgana <3931767878@qq.com>
2025-10-03 10:16:20 +0800

    重要更新，BUG几乎清除

---
+++ src/main/java/com/morgana/thevoidslawoverturetoreboot/effects/ModFoodsEffects.java
@@ -0,0 +1,13 @@
+package com.morgana.thevoidslawoverturetoreboot.effects;
+
+import com.morgana.thevoidslawoverturetoreboot.TheVoidsLawOvertureReboot;
+import net.minecraft.world.effect.MobEffect;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.neoforged.neoforge.registries.NeoForgeRegistries;
+
+public class ModFoodsEffects {
+    public static final DeferredRegister<MobEffect> FOOD_EFFECTS =
+        DeferredRegister.create(NeoForgeRegistries.MOD_E, TheVoidsLawOvertureReboot.MOD_ID);
+
+    public static final
+}
